e750a894785a317709f364ab61fa9b4d41f80f0c
a.dambrine <art-dambrine@live.fr>
2020-08-21 17:40:31 +0200

    Config de encore et vuejs

---
+++ assets/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Composant Vue APP</h2>
+        <p>Mon URL d'API (.env) : {{urlApi}}</p>
+        <button class="btn btn-primary" @click="consolelog">Hello</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data () {
+            return {
+                urlApi: process.env.VUE_APP_API_URL
+            }
+        },
+        methods : {
+             consolelog() {
+                console.log("Hello vue")
+             }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
